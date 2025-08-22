5cccbe4b812f8c6e0fb32592da564b3858ee23d2
Chaitanya459 <chaitanyatanneeru459@gmail.com>
2025-08-22 14:54:27 -0400

    Commit after Comments 	modified:   packages/api/src/application/features/assessments/create/useCase.ts

--- packages/api/src/application/features/assessments/create/useCase.ts
+++ packages/api/src/application/features/assessments/create/useCase.ts
@@ -1,8 +1,7 @@
-import { error } from 'console';
 import { inject, injectable } from 'inversify';
 import { IUseCase } from 'src/types/shared';
 import { Assessment, CreateAssessmentDTO } from 'src/types';
-import { NUMBER } from 'sequelize';
+
 import { IAssessmentRepository } from '../../../contracts';
 
 @injectable()
